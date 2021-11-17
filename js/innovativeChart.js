4088e91ba4037f79a1fffe66c5053dc775b76892
Somya-Bansal <somyabansal1996@gmail.com>
2021-11-17 11:01:22 -0700

    Cleanup innovative chart code

--- js/innovativeChart.js
+++ js/innovativeChart.js
@@ -1,27 +1,22 @@
 function drawInnovativeChart(friData) {
 
-    locations = [
-        "Entry Corridor", "Kiddie Land", "Tundra Land", "Wet Land", "Coaster Alley"
-    ]
+    let innovativeSvg = d3.select("#visInnovative");
+    width = +innovativeSvg.style('width').replace('px', '');
+    height = +innovativeSvg.style('height').replace('px', '');
     const margin = { top: 10, right: 10, bottom: 10, left: 10 },
-        width = 900 - margin.left - margin.right,
-        height = 900 - margin.top - margin.bottom;
+        innerWidth = width - margin.left - margin.right,
+        innerHeight = height - margin.top - margin.bottom;
+
+    locations = ["Entry Corridor", "Kiddie Land", "Tundra Land", "Wet Land", "Coaster Alley"]
+    const colorScale = d3.scaleOrdinal(d3.schemeTableau10).domain(locations);
+
     res = d3.rollup(
         friData.filter((it) => new Date(it.Timestamp).getHours() <= 24),
         (d) => d.length,
         (d) => d.location,
         (d) => d.from
     );
-
     console.log(res);
-
-    const svg = d3
-        .select('#vis1')
-        .append('svg')
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
-        .append('g')
-        .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
     childrenAccessorFn = ([key, value]) => value.size && Array.from(value);
     hierarchyData = d3
@@ -29,23 +24,19 @@ function drawInnovativeChart(friData) {
         .sum(([, value]) => value)
         .sort((a, b) => b.value - a.value);
 
-    pack = d3.pack().size([width, height]).padding(1)(hierarchyData);
+    const root = d3.pack().size([width, height]).padding(0.5)(hierarchyData);
 
-    const root = pack;
 
-    var color = d3.scaleOrdinal()
-        .domain(locations)
-        .range(["#264653", "#2A9D8F", "#E9C46A", "#F4A261", "E76F51"])
-    svg
+    innovativeSvg
         .append('g')
         .selectAll('circle')
         .data(root.leaves())
         .join('circle')
-        .attr('transform', (d) => `translate(${d.x},${d.y})`)
         .attr('r', (d) => d.r)
-        .style("fill", function (d) { return color(d.parent.data[0]) })
+        .attr('transform', (d) => `translate(${d.x},${d.y})`)
         .attr('opacity', '0.3')
-        .append('title');
+        .style("fill", function (d) { return colorScale(d.parent.data[0]) })
+        // .append('title');
     //   .text((d) => {
     //     // console.log(d);
     //     return `${d.ancestors().map((d) => d.data[0])}`;
@@ -53,7 +44,7 @@ function drawInnovativeChart(friData) {
     //     //   .join('/')}\n${format(d.value)}`;
     //   });
 
-    svg
+    innovativeSvg
         .append('g')
         .attr('pointer-events', 'none')
         .attr('text-anchor', 'middle')
