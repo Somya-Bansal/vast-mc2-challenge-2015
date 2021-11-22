992933425378d720b3c50a33b0942af44808a2cf
Kiran Prasad <kiran.prasad@hotmail.com>
2021-11-21 18:28:33 -0700

    Added support for all days + cosmetic changes to axes and plots. Tooltip selections now work.

--- js/bar.js
+++ js/bar.js
@@ -1,8 +1,10 @@
 
+var ReceiverId_freq={};
+var SenderId_freq={};
+var selected_frame = null;
 
 function computeStats(arr, Nstdev=1){
 
-    console.log("computing stats")
     sum = arr.reduce((a,b)=>a+b,0)
     mean = (sum/arr.length) || 0;
     stdev_num = 0
@@ -21,7 +23,7 @@ function drawBar(day, loc, outlier_flag, extcomm_flag){
 
     switch (day) {
         case "1":
-            day_data = fridata;
+            day_data = Array.prototype.concat(fridata,saturdata,sundata);
             break;
         case "2":
             day_data = fridata;
@@ -35,11 +37,11 @@ function drawBar(day, loc, outlier_flag, extcomm_flag){
         default:
             day_data = fridata;
             break;
-    }
+    }    
     var loc_;
     switch (loc) {
         case "1":
-            loc_ = "Entry Corridor";
+            loc_ = "All";
             break;
         case "2":
             loc_ = "Entry Corridor";
@@ -61,11 +63,11 @@ function drawBar(day, loc, outlier_flag, extcomm_flag){
             break;
     }
 
-    day_data = day_data.filter((d)=>{
-        return d.Location==loc_;
-    })
-
-    console.log(day, loc_, outlier_flag, extcomm_flag, day_data)
+    if (loc_ != 'All'){
+        day_data = day_data.filter((d)=>{
+            return d.Location==loc_;
+        })
+    }
 
     // filter on extcomm flag here
     if (!extcomm_flag){
@@ -74,9 +76,8 @@ function drawBar(day, loc, outlier_flag, extcomm_flag){
         })
     }
     
-
-    var ReceiverId_freq=[];
-    var SenderId_freq=[];
+    SenderId_freq = {};
+    ReceiverId_freq = {};
     var sIds = [];
     var rIds = [];
 
@@ -107,63 +108,26 @@ function drawBar(day, loc, outlier_flag, extcomm_flag){
     sIds = sIds.slice(0,30);
     rIds = rIds.slice(0,30);
 
-    barchart(sIds, SenderId_freq, barSvg, 'barchart');
-    barchart(rIds, ReceiverId_freq, barSvg2, 'barchart2')
-
-
-    // let xoffset=60;
-    // let yoffset=30;
-
-    // xScale = d3.scaleLinear()
-    //         .domain([0,SenderId_freq[sIds[0]]])
-    //            .range([xoffset,1000]);
-
-    // yScale = d3.scaleBand()
-    //             .domain(sIds)
-    //             .range([yoffset,500]).padding(0.1);
-
-    // xAxis = d3.axisTop(xScale).tickSizeOuter(0);
-    // yAxis = d3.axisLeft(yScale).tickSizeOuter(0);
-
-    // barSvg.selectAll('g').remove();
-    
-    // let g=barSvg.append('g');
-
-    // g.selectAll('g')
-    // .data(sIds)
-    // .enter().append("rect")
-    //   .attr("x", xScale(0))
-    //   .attr("y", function(d) { return yScale(d); })
-    //   .attr("width", function(d) {return xScale(SenderId_freq[d])})
-    //   .attr("height", yScale.bandwidth())
-    //   .style('fill',"blue");
-
-    // g.append('g')
-    // .attr('transform', `translate(${0}, ${yoffset})`)
-    // .call(xAxis);
-
-    // g.append('g')
-    // .attr('transform', `translate(${xoffset}, ${0})`)  
-    // .call(yAxis);
+    barchart(sIds, SenderId_freq, barSvg, 'barchart', ylabel='Sender ID', xlabel='Frequency');
+    barchart(rIds, ReceiverId_freq, barSvg2, 'barchart2', ylabel='Receiver ID', xlabel='Frequency' )
 
 }
 
-function barchart(data, freq, svgelement, id){
+function barchart(data, freq, svgelement, id, xlabel='', ylabel=''){
 
-
-    let xoffset=60;
-    let yoffset=30;
+    let xoffset=25;
+    let yoffset=50;
 
     xScale = d3.scaleLinear()
             .domain([0,freq[data[0]]])
-               .range([xoffset,1000]);
+               .range([xoffset,500]);
 
     yScale = d3.scaleBand()
                 .domain(data)
                 .range([yoffset,500]).padding(0.1);
 
     xAxis = d3.axisTop(xScale).tickSizeOuter(0);
-    yAxis = d3.axisLeft(yScale).tickSizeOuter(0);
+    yAxis = d3.axisRight(yScale).tickSizeInner(0);
 
     let element='g#'+id;
     let xname = 'g#xscale_'+id;
@@ -171,7 +135,7 @@ function barchart(data, freq, svgelement, id){
     svgelement.selectAll(element).remove();
     svgelement.selectAll(xname).remove();
     svgelement.selectAll(yname).remove();
-    
+
     let g=svgelement
             .append('g')
             .attr('id',id);
@@ -180,20 +144,119 @@ function barchart(data, freq, svgelement, id){
     .data(data)
     .enter()
     .append("rect")
+      .attr('id',id)
       .attr("x", xScale(0))
       .attr("y", function(d) { return yScale(d); })
       .attr("width", function(d) {return xScale(freq[d])})
       .attr("height", yScale.bandwidth())
-      .style('fill',"blue");
+      .style('fill',"#b07aa1")
+      .on('mouseover', mouseoverFunc)
+      .on('mousemove', mousemoveFunc)
+      .on('mouseout', mouseoutFunc)
+      .on('click', mouseclickFunc);
 
-    g.append('g')
+    let x = g.append('g')
     .attr('id',xname)
     .attr('transform', `translate(${0}, ${yoffset})`)
     .call(xAxis);
 
-    g.append('g')
+    let y = g.append('g')
     .attr('id',yname)
     .attr('transform', `translate(${xoffset}, ${0})`)  
     .call(yAxis);
 
+    y.selectAll(".tick text").attr("fill","black").attr('font-size','10px');
+
+    g.append("text")
+            .attr("id", "label-y")
+            .attr("class", "x-label")
+            .attr("fill",'gray')
+            .attr("text-anchor", "middle")
+            .attr("x", 250)
+            .attr("y", yoffset/2)
+            .text(ylabel);
+  
+    g.append('text')
+            .attr("id", "label-x")
+            .attr("class", "y-label")
+            .attr("fill",'gray')
+            .attr('transform', 'rotate(-90)')
+            .attr("text-anchor", "middle")
+            .attr("x", -250)
+            .attr("y", 15)
+            .text(xlabel);
+
+}
+
+function getfreqData(id){
+    if (id == 'barchart'){
+        return [SenderId_freq, 'sender'];
+    }
+    else{
+        return [ReceiverId_freq, 'receiver'];
+    }
+}
+
+function mouseoverFunc(d){
+    var data;
+    data = getfreqData(d3.select(this).attr('id'));
+    d3.select(this)
+        .classed("highlightedBar",true);
+        div.transition()
+           .duration(50)
+           .style("opacity", 1);
+    if (d == -1){
+        d = 'external';
+    }
+    div.html("User ID: " +d+"<br/>Frequency: "+ data[0][d])
+    .style("left", (d3.event.pageX + 10) + "px")
+    .style("top", (d3.event.pageY - 15) + "px");
+
+}
+function mouseclickFunc(d){
+    var data;
+    data = getfreqData(d3.select(this).attr('id'));
+    selected_userID = d;
+    commType = data[1];
+
+    if (selected_frame != null){
+        d3.select(selected_frame)
+    .classed("highlightedBar",false);
+    //Makes the new div disappear:
+    div.transition()
+    .duration('50')
+    .style("opacity", 0);
+    }
+
+    selected_frame = this;
+
+    d3.select(this)
+        .classed("highlightedBar",true);
+        div.transition()
+           .duration(50)
+           .style("opacity", 1);
+}
+function mouseoutFunc(d){
+    if (selected_frame == null){
+        d3.select(this)
+    .classed("highlightedBar",false);
+    //Makes the new div disappear:
+    div.transition()
+    .duration('50')
+    .style("opacity", 0);
+    }
+    else if(this!=selected_frame){
+        d3.select(this)
+    .classed("highlightedBar",false);
+    //Makes the new div disappear:
+    div.transition()
+    .duration('50')
+    .style("opacity", 0);
+    }
+}
+
+function mousemoveFunc(d){
+
+      div.style('left', (d3.event.pageX+10) + 'px')
+         .style('top', (d3.event.pageY+10) + 'px')
 }
--- js/main.js
+++ js/main.js
@@ -6,7 +6,9 @@ var wknd;
 var loc;
 var outliers;
 var extcomm;
-
+var div;
+var selected_userID=null;
+var commType=null;
 var fridata=[];
 var saturdata=[];
 var sundata=[];
@@ -31,30 +33,27 @@ document.addEventListener('DOMContentLoaded', function() {
             Location: d.location
         }
     }
-    date = new Date();
-    let start=date.getTime();
-    // Load both files before doing anything else
+    
+    div = d3.select("body").append("div")
+    .attr("class", "tooltip-cmap")
+    .style("opacity", 0);
+
     Promise.all([
                 d3.csv('data/comm-data-Fri.csv',
                         rowConverter
                         ),
-                // d3.csv('data/comm-data-Sat.csv',
-                //         rowConverter
-                //         ),
-                // d3.csv('data/comm-data-Sun.csv',
-                //         rowConverter
-                //         )
+                d3.csv('data/comm-data-Sat.csv',
+                        rowConverter
+                        ),
+                d3.csv('data/comm-data-Sun.csv',
+                        rowConverter
+                        )
                 ])
             .then(function(values){
         
             fridata=values[0];
-            // saturdata=values[1];
-            // sundata=values[2];
-            date = new Date();
-            let end=date.getTime();
-            let time=end-start;
-            console.log("loaded")
-            console.log(time)
+            saturdata=values[1];
+            sundata=values[2];
             render_chart();
     })
     
